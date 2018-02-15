f6576a1bca29505303d5c74dc3ac8035644ea7a0
Crystal Wang <cwang4@stuy.edu>
2018-02-14 22:48:21 -0500

    Initial commit

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,118 @@
+public class KnightBoard{
+  private int[][] board;
+
+  public KnightBoard(int startingRows, int startingCols){
+      if (startingCols < 0 || startingRows < 0){
+        throw new IllegalArgumentException();
+      }
+      board = new int[startingRows][startingCols];
+  }
+
+  public String toString(){
+    String ans = "";
+    for (int i = 0; i < board.length; i ++){
+      for (int j = 0; j < board[i].length; j++){
+        if (board.length * board[0].length >= 10 && board[i][j] < 10){
+            ans += "_";
+        }
+        if(board[i][j] != 0){
+          ans += board[i][j] + " ";
+        }else{
+          ans += "_ ";
+        }
+      }
+      ans += "\n";
+    }
+    return ans;
+  }
+
+  public boolean solve(int startingRow, int startingCol){
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[i].length; j++){
+        if (board[i][j] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+
+    if (startingRow < 0 || startingCol < 0){
+      throw new IllegalArgumentException();
+    }
+    return solveH(startingRow, startingCol, 1);
+  }
+
+  private boolean solveH(int row, int col, int level){
+    int[][] coordinates = {{2,1},{1,2},{-2,1},{-1,2},
+                           {2,-1},{1,-2},{-1,-2},{-2,-1}};
+    if (level == board.length * board[0].length){
+      board[row][col] = level;
+      return true;
+    }
+    for(int[] x: coordinates){
+      try{
+        if (board[row + x[0]][col + x[1]] == 0){
+          board[row][col] = level;
+          if(solveH(row + x[0], col + x[1], level + 1)){
+            return true;
+          }
+          else{
+            board[row][col] = 0;
+          }
+        }
+      }catch(Exception e){
+
+      }
+    }
+    return false;
+  }
+
+  public int countSolutions(int startingRow, int startingCol){
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[i].length; j++){
+        if (board[i][j] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+
+    if (startingRow < 0 || startingCol < 0){
+      throw new IllegalArgumentException();
+    }
+    return countHelp(startingRow, startingCol, 1);
+
+  }
+
+  public int countHelp(int row, int col, int level){
+    int sol = 0;
+    int[][] coordinates = {{2,1},{1,2},{-2,1},{-1,2},
+                           {2,-1},{1,-2},{-1,-2},{-2,-1}};
+    if (level == board.length * board[0].length){
+      return 1;
+    }
+    for(int[] x: coordinates){
+      if(row + x[0] < board.length && row + x[0] >= 0 &&
+         col + x[1] < board[0].length && col + x[1] >= 0){
+        if (board[row + x[0]][col + x[1]] == 0){
+          board[row][col] = level;
+          sol += countHelp(row + x[0], col + x[1], level + 1);
+          board[row][col] = 0;
+          }
+      }
+    //  System.out.println(sol);
+    }
+    return sol;
+  }
+
+  public static void main(String[] args){
+    KnightBoard b = new KnightBoard(5,5);
+    System.out.println(b);
+    System.out.println(b.countSolutions(0,0));
+  /*  for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        KnightBoard a = new KnightBoard(5,5);
+        System.out.println(a.solve(i,j));
+      }
+    }*/
+
+  }
+}
