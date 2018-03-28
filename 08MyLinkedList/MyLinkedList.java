f7a6fc2b108d50dd03a4a175a3f4952feffe2fb5
Crystal Wang <cwang4@stuy.edu>
2018-03-28 16:15:25 -0400

    Initial commit

---
+++ 08MyLinkedList/MyLinkedList.java
@@ -0,0 +1,24 @@
+public class MyLinkedList{
+    private Node start, end;
+    private int size;
+
+
+    public MyLinkedList(){
+    }
+
+    private class Node{
+	private Integer data;
+	private Node next;
+
+	private Node(){
+	}
+
+	
+    }
+
+
+
+
+
+    
+}
